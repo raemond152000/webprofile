942eb7b072fe1f19d4ca6ba4147f733ce036237f
raemond <raemondcarta@gmail.com>
2023-01-29 23:16:30 -0500

    remake

---
+++ app.js
@@ -0,0 +1,58 @@
+const boxContainer = document.querySelector(".boxContainer")
+const saluteTextContainer = document.querySelector(".saluteTextContainer")
+const saluteImgContainer = document.querySelector(".saluteImgContainer")
+const authorname = document.querySelector(".name")
+const authorjob = document.querySelector(".job")
+const jobTitleContainers = document.querySelectorAll(".jobTitleContainer")  //because there are two containers 
+const jobTitles = document.querySelectorAll(".jobTitle")  
+const jobDesc = document.querySelector(".jobDesc")  
+const projectTitle = document.querySelector(".projectTitle")  
+const phones = document.querySelectorAll(".phone")  
+const laptop = document.querySelector(".laptop")  
+
+
+
+for(let i = 0 ; i<365; i++){
+
+  const list = [
+    0, 1, 2, 3, 41, 42, 43, 44, 82, 83, 123, 124, 125, 126, 164, 165, 166, 167,
+    205, 206, 246, 247, 287, 288, 5, 6, 46, 47, 87, 88, 128, 129, 169, 170, 210,
+    211, 251, 252, 292, 293, 8, 9, 14, 15, 49, 50, 51, 90, 91, 92, 131, 132,
+    133, 134, 172, 173, 213, 214, 254, 255, 295, 296, 175, 176, 217, 177, 218,
+    259, 55, 56, 96, 97, 137, 138, 178, 179, 219, 220, 260, 261, 217, 218, 259,
+    260, 261, 301, 302, 17, 18, 19, 58, 59, 60, 61, 99, 100, 102, 103, 140, 141,
+    144, 181, 182, 185, 222, 223, 225, 226, 263, 264, 265, 266, 304, 305, 306,
+    24, 25, 65, 66, 67, 106, 107, 108, 147, 148, 149, 150, 188, 189, 229, 230,
+    270, 271, 311, 312, 67, 108, 149, 150, 191, 232, 192, 233, 274, 234, 275,
+    316, 194, 235, 276, 154, 195, 236, 73, 114, 155, 33, 34, 74, 75, 115, 116,
+    156, 157, 197, 198, 238, 239, 279, 280, 320, 321, 36, 37, 38, 39, 77, 78,
+    79, 80, 118, 119, 159, 160, 161, 162, 200, 201, 202, 203, 241, 242, 282,
+    283, 284, 285, 323, 324, 325, 326,
+  ];
+
+
+    const el = document.createElement("div")
+    el.classList = list.includes(i) ? "box active" : "box"   //if list includes i make it active if not just the box (class = box active)
+    boxContainer.appendChild(el)
+}
+
+window.addEventListener("scroll", ()=>{
+  //console.log(window.scrollY);   // gives you the coordinates for horizontal disposition in scrolling
+  let offsetY = window.scrollY
+  saluteTextContainer.style.transform=`translateY(${offsetY * 0.3}px)`  //position of container saluteText increases as you scroll so  // so the effect will be it seems it does not move even when you scroll
+  saluteImgContainer.style.transform=`translate(${offsetY * 0.4}px, ${offsetY * 0.7}px)`  //you can set it as translate X,Y axis
+                                                                         // multiplying it to a factor will affects it speed of movement
+  authorname.style.transform=`translateX(${offsetY * 0.5}px)`   
+  authorjob.style.transform=`translateX(calc(100vh - ${offsetY}px))`  
+  
+  jobTitleContainers[0].style.backgroundPositionY = `${offsetY * 0.5}px`  //1st element of container //creating parallax effect                                                                      
+  jobTitleContainers[1].style.backgroundPositionY = `${-offsetY * 0.5}px`  //2nd element of container //creating parallax effect                                                                      
+                                                                    
+/*   job.style.transform = `translateX(calc(0vh - ${offsetY}px))`   //there are two 100vh containers ahead this element  */
+  jobTitles[0].style.transform = `translateY(calc(-200vh + ${offsetY}px))`   //there are two 100vh containers ahead this element 
+  jobDesc.style.transform = `translateY(calc(-300vh + ${offsetY}px))`   //there are three 100vh containers ahead this element 
+  projectTitle.style.transform = `translateY(calc(400vh - ${offsetY}px))` 
+  /* phones[0].style.transform = `translateX(calc(500vh - ${offsetY}px))` 
+  laptop.style.transform = `translateX(calc(600vh - ${offsetY}px))` 
+  phones[1].style.transform = `translateX(calc(750vh - ${offsetY}px))`  */
+})
